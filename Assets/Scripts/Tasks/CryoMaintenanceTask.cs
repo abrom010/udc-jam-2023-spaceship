bb25f77ebb2ec06227bf153ea3c076a8ea43c064
Aaron <abrom010@fiu.edu>
2023-06-05 23:36:55 -0400

    cryo task update

--- Assets/Scripts/Tasks/CryoMaintenanceTask.cs
+++ Assets/Scripts/Tasks/CryoMaintenanceTask.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class CryoMaintenanceTask : Interactable
 {
     [SerializeField] GameManager gameManager;
+    [SerializeField] ParticleSystem particleSystem;
 
     public override void Interact()
     {
-        gameManager.spaceShip.cryoManager.SetCryoPercentage(100f);
+        gameManager.spaceShip.cryoManager.SetCryoPercentage(gameManager.spaceShip.cryoManager.GetCryoPercentage() >= 100f ? 100f : gameManager.spaceShip.cryoManager.GetCryoPercentage() + 1f);
+        particleSystem?.Play();
     }
 }
