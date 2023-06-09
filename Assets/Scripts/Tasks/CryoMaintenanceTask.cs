5f8acfce28ab5762db9cf6ab09286c982dfaa4f8
Aaron <abrom010@fiu.edu>
2023-06-09 06:09:27 -0400

    putting stuff together, added sounds

--- Assets/Scripts/Tasks/CryoMaintenanceTask.cs
+++ Assets/Scripts/Tasks/CryoMaintenanceTask.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class CryoMaintenanceTask : Interactable
 {
-    [SerializeField] ParticleSystem particleSystem;
+    [SerializeField] ParticleSystem particles;
+
+    public AudioClip iceCubeClip;
 
     public override void Interact(bool primary)
     {
         GameManager.instance.spaceShip.cryoManager.SetCryoPercentage(GameManager.instance.spaceShip.cryoManager.GetCryoPercentage() >= 100f ? 100f : GameManager.instance.spaceShip.cryoManager.GetCryoPercentage() + 1f);
-        particleSystem?.Play();
+        particles?.Play();
+
+        AudioSource.PlayClipAtPoint(iceCubeClip, transform.position, 0.5f);
     }
 }
